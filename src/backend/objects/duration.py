ef4d8b0a0a84165d19363ffb30e8477558686d7a
gustavogld <gustavo.relativistico@gmail.com>
2024-09-25 18:19:30 -0300

    feat(DurationObj): to_minutes()

--- src/backend/objects/duration.py
+++ src/backend/objects/duration.py
@@ -8,5 +8,8 @@ class DurationObj(BaseModel):
     minutes: Optional[int] = Field(default=None)
     seconds: Optional[int] = Field(default=None)
 
+    def to_minutes(self):
+        return self.hours * 60 + self.minutes  # + self.seconds / 60
+
     def __str__(self):
         return f"{self.hours}h {self.minutes}m {self.seconds}s"

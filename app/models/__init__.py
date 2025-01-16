86046d4123cc49904b34219ac0f4302db833793c
gustavogld <gustavo.relativistico@gmail.com>
2025-01-16 17:50:49 -0300

    add(models): SurgeryPossibleRooms

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,3 +5,4 @@ from app.models.room import Room
 from app.models.surgery import Surgery
 from app.models.team import Team
 from app.models.surgery_possible_teams import SurgeryPossibleTeams
+from app.models.surgery_possible_rooms import SurgeryPossibleRooms

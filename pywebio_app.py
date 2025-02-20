2dfa1b87b27813b7e982daf081e91a5314388ef4
gustavogld <gustavo.relativistico@gmail.com>
2025-02-20 17:23:39 -0300

    edit(CRUDTable): add gen_data_func gen_data_func edit_func del_func

--- pywebio_app.py
+++ pywebio_app.py
@@ -14,17 +14,13 @@ from functools import partial
 
 
 class CRUDTable:
-    def __init__(self, gen_data_func, edit_func, del_func):
-        
-        self.datatable = gen_data_func()
-        self.gen_data_func = gen_data_func
-        self.edit_func = edit_func
-        self.del_func = del_func
+    def __init__(self):
+        self.datatable = self.gen_data_func()
 
     def put_crud_table(self):
         # the CRUD table without the header
         table = []
-        
+
         for i, table_row in enumerate(self.datatable):
             table_row = [put_text(row_element[1]) for row_element in table_row] + [
                 # use i - 1 here so that it counts after the header row.
@@ -32,45 +28,106 @@ class CRUDTable:
                 put_buttons(["✖️"], onclick=partial(self.handle_edit_delete, custom_func=self.del_func, i=i))
             ]
             table.append(table_row)
-        
+
         with use_scope("table_scope", clear=True):
             put_table(table,
-                header=list(self.datatable[0].model_dump().keys()) + ["Edit", "Delete"]
-            )
+                      header=list(self.datatable[0].model_dump().keys()) + ["Edit", "Delete"]
+                      )
+
+            put_row([
+                put_button("Adicionar novo registro", onclick=partial(self.add_func, table=self.datatable)),
+                None,
+                put_button("Atualizar tabela", onclick=lambda: run_js('window.location.reload()')),
+            ])
+
 
     def handle_edit_delete(self, dummy, custom_func, i):
         '''when edit/delete button is pressed, execute the custom edit/delete
         function as well as update CRUD table'''
 
-        # originally had it in the custom functions in step5_filemanager.py, 
+        # originally had it in the custom functions in step5_filemanager.py,
         # but thought its probably best to have it within the crud_table class to
         # requery all the filepaths and refresh the crud_table
-        
+
         if custom_func == self.edit_func:
             self.datatable = custom_func(self.datatable, i)
             # refresh table output
             self.put_crud_table()
-        
+
         # if it's the delete function, ask for confirmation
         if custom_func == self.del_func:
-            
+
             # melt the data (row becomes key, value)
             datatable_melt = list(zip(self.datatable[0], self.datatable[i]))
             popup(
                 '⚠️ Are you sure you want to delete?',
                 [
                     put_table(datatable_melt, header=["row", "data"]),
-                    put_buttons(['confirm', 'cancel'], 
-                    onclick = lambda x: self.handle_confirm(i) if x == 'confirm' else close_popup())
+                    put_buttons(['confirm', 'cancel'],
+                                onclick = lambda x: self.handle_confirm(i) if x == 'confirm' else close_popup())
                 ]
             )
-    
+
     def handle_confirm(self, i):
         ''' if confirm button pressed in deletion confirmation, delete, and also close popup'''
         self.datatable = self.del_func(self.datatable, i)
         close_popup()
         # refresh table output
         self.put_crud_table()
+
+    def gen_data_func(self):
+        with Session(get_engine()) as session:
+            return session.query(Surgery).all()
+
+    def get_primary_key(self, model) -> str:
+        """Retorna o nome da chave primária da tabela do modelo informado."""
+        return inspect(model).primary_key[0].name  # Obtém dinamicamente a chave primária
+
+    def edit_func(self, table, i):
+        """Edita um registro no banco de dados baseado na entrada do usuário."""
+        if i == 0:  # Evita editar o cabeçalho da tabela
+            return table
+
+        primary_key = self.get_primary_key(Surgery)
+        record = table[i]  # Obtém o registro da linha selecionada
+        record_dict = record.model_dump()  # Converte para dicionário
+
+        # Pergunta qual campo editar
+        field_to_edit = select("Selecione o campo para editar:", record_dict.keys())
+        if not field_to_edit:
+            return table  # Se o usuário cancelar, não faz nada
+
+        new_value = input(f'Novo valor para {field_to_edit}:', value=str(record_dict[field_to_edit]))
+
+        # Atualiza o banco de dados
+        with Session(get_engine()) as session:
+            obj = session.get(Surgery, record_dict[primary_key])
+            setattr(obj, field_to_edit, new_value)  # Atualiza o campo
+            session.commit()
+
+        # Atualiza a tabela na interface
+        table[i] = session.get(Surgery, record_dict[primary_key])
+        return table
+
+    def del_func(self, table, i):
+        table.pop(i)
+        return table
+
+    def add_func(self, table):
+        """Adiciona um novo registro ao banco de dados."""
+        new_record = Surgery(
+            name=input("Nome da cirurgia:", required=True),
+            duration=input("Duração da cirurgia:", type=NUMBER, required=True),
+            priority=input("Prioridade da cirurgia:", type=NUMBER, required=True),
+            patient_id=input("ID do paciente:", type=NUMBER, required=False)
+        )
+
+        with Session(get_engine()) as session:
+            session.add(new_record)
+            session.commit()
+
+        table.append(new_record)
+        return table
 
 
 DATABASE_URL = "sqlite:///database.db"
@@ -87,47 +144,6 @@ def get_engine():
     return engine
 
 
-def generate_datatable():
-    with Session(get_engine()) as session:
-        return session.query(Surgery).all()
-
-
-def get_primary_key(model) -> str:
-    """Retorna o nome da chave primária da tabela do modelo informado."""
-    return inspect(model).primary_key[0].name  # Obtém dinamicamente a chave primária
-
-
-def edit_table(table, i):
-    """Edita um registro no banco de dados baseado na entrada do usuário."""
-    if i == 0:  # Evita editar o cabeçalho da tabela
-        return table
-
-    primary_key = get_primary_key(Surgery)
-    record = table[i]  # Obtém o registro da linha selecionada
-    record_dict = record.model_dump()  # Converte para dicionário
-
-    # Pergunta qual campo editar
-    field_to_edit = select("Selecione o campo para editar:", record_dict.keys())
-    if not field_to_edit:
-        return table  # Se o usuário cancelar, não faz nada
-
-    new_value = input(f'Novo valor para {field_to_edit}:', value=str(record_dict[field_to_edit]))
-
-    # Atualiza o banco de dados
-    with Session(get_engine()) as session:
-        obj = session.get(Surgery, record_dict[primary_key])
-        setattr(obj, field_to_edit, new_value)  # Atualiza o campo
-        session.commit()
-
-    # Atualiza a tabela na interface
-    table[i] = session.get(Surgery, record_dict[primary_key])
-    return table
-
-
-def delete_table(table, i):
-    table.pop(i)
-    return table
-
 
 def main():
 
@@ -135,7 +151,7 @@ def main():
 
     # Header
     # datatable = [header, row1, row2, row3] for the crud table
-    growth_table = CRUDTable(gen_data_func=generate_datatable, edit_func=edit_table, del_func=delete_table)
+    growth_table = CRUDTable()
     growth_table.put_crud_table()
 
 
